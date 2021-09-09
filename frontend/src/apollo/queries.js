cd9e98fc095339abf120c3f442b281945d600063
Aamir Alam <aamiralam1991@gmail.com>
2021-09-09 17:54:17 +0530

    Add top pool graphql query

--- frontend/src/apollo/queries.js
+++ frontend/src/apollo/queries.js
@@ -1,4 +1,5 @@
 import { gql } from "@apollo/client";
+import { getUnixTime } from "../utils/helper";
 import client from "./client";
 
 export const testQuery = async () => {
@@ -18,19 +19,124 @@ export const testQuery = async () => {
   console.log(res.data);
 };
 
-export const getTopTokens = async () => {
-  const queryObj = `
+export const getTopTokens = async (page = 1) => {
+  const items = page * 10;
+  const skips = page * 10 - 10;
+
+  try {
+
+    const queryObj = `
     query {
-        depositEntities(first: 5) {
+        tokens(first: ${items}, skip: ${skips} ) {
           id
-          fromAccount
-          amount
-          depositedAt
+          name
+          symbol
+          tradeVolume
+          tradeVolumeUSD
+          totalLiquidity
+          derivedETH
         }
-      }
+     }
     `;
 
-  const res = await client.query({ query: gql(queryObj) });
+    const res = await client.query({ query: gql(queryObj) });
+    return res.data.tokens
 
-  console.log(res.data);
+  } catch (error) {
+    console.log('getTopTokens', error)
+    return []
+  }
+
+
 };
+
+// top pools based on reserved ETH in the pool
+export const topPools = async (order = "desc", page = 1) => {
+  const items = 10 * page;
+  const skips = 10 * page - 10;
+
+  try {
+
+    const queryObj = `
+            query {
+              pairs(first: ${items}, skip: ${skips}, orderBy: reserveETH orderDirection: ${order}){
+              id
+              token0 {
+                symbol
+              }
+              token1 {
+                symbol
+              }
+              reserveETH
+              reserveUSD
+            }
+          }
+    `
+    const res = await client.query({ query: gql(queryObj) })
+    return res.data.pairs
+  } catch (error) {
+    return []
+  }
+
+}
+
+
+// pair volume 24: pairDayData(pairId, 1 )
+// pair volume 7d pairDayData(pairId, 7 )
+export const pairDayData = async (pairId, days) => {
+
+  const todayUnixTime = getUnixTime(0);
+
+
+  try {
+    const queryObj =
+      ` 
+          query {
+            pairDayDatas(first: ${days}, orderBy: date, orderDirection: desc,
+              where: {
+                pairAddress: "${pairId}",
+                date_lt: ${todayUnixTime}
+              }
+            ) {
+                date
+                dailyVolumeToken0
+                dailyVolumeToken1
+                dailyVolumeUSD
+                reserveUSD
+            }
+          }
+      `
+    const res = await client.query({ query: gql(queryObj) });
+    return res.data.pairDayDatas;
+
+  } catch (error) {
+    return []
+  }
+
+}
+
+// PolkabridgeAmmDailyData
+
+export const polkabridgeAmmDailyData = async (days = 128) => {
+
+  try {
+    const queryObj = `
+      query {
+          uniswapDayDatas (first: 7, orderBy: date orderDirection: desc ) {
+            date
+            dailyVolumeETH
+            dailyVolumeUSD
+            totalVolumeETH
+            totalLiquidityETH
+            totalLiquidityUSD
+            txCount
+          }
+          }
+    
+    `
+    const res = await client.query({ query: gql(queryObj) });
+    return res.data.uniswapDayDatas;
+  } catch (error) {
+    return []
+  }
+}
