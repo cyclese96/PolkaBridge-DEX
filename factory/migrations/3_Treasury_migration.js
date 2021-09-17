ff31e56ddffd1df1799e5cfdbedb0c02910f2f6e
cyclese96 <cycle96@gmail.com>
2021-09-17 15:41:20 +0700

    delete

--- factory/migrations/3_Treasury_migration.js
+++ factory/migrations/3_Treasury_migration.js
@@ -3,8 +3,8 @@ const Spac = artifacts.require("test/Spac")
 
 const Treasury = artifacts.require("PolkaTreasury")
 
-module.exports = function(deployer, network, accounts) {
-    deployer.deploy(Tend)
-    deployer.deploy(Spac)
-    deployer.deploy(Treasury, accounts[0])
+module.exports = function (deployer, network, accounts) {
+    // deployer.deploy(Tend)
+    // deployer.deploy(Spac)
+    // deployer.deploy(Treasury, accounts[0])
 }
