903a4c2b9e6ea71855d47411a63062178abe4566
Aamir Alam <aamiralam1991@gmail.com>
2021-10-30 18:16:02 +0530

    Add subgraph mainnet config

--- frontend/src/apollo/client.js
+++ frontend/src/apollo/client.js
@@ -1,12 +1,14 @@
 import { ApolloClient } from "apollo-client";
 import { InMemoryCache } from "apollo-cache-inmemory";
 import { HttpLink } from "apollo-link-http";
+import { currentConnection } from '../constants';
 // import { HttpLink } from 'apollo-link-http'
 // const APIURL =
 // "https://gateway.thegraph.com/api/584823fe20b487773537ceae7d79941b/subgraphs/id/0x4bb4c1b0745ef7b4642feeccd0740dec417ca0a0-0";
 // const APIURL = "https://api.studio.thegraph.com/query/8207/amm/0.8.1.2"; // oldsubgraph working price api
 // const APIURL = "https://api.studio.thegraph.com/query/2992/pbr-dex/2.3"//last recent factory
 const APIURL = "https://api.studio.thegraph.com/query/8207/amm/0.9.8"; // current latest subgraph
+const API_MAINNET = "https://api.studio.thegraph.com/query/2992/pbr-dex/3.0.0";
 const BLOCKS_API_TESTNET =
   "https://api.studio.thegraph.com/query/8207/rinkeby-blocks/0.0.1";
 const BLOCKS_API_MAINNET =
@@ -41,16 +43,18 @@ export const healthClient = new ApolloClient({
   shouldBatch: true,
 });
 
+const _blocksApi = currentConnection == 'testnet' ? BLOCKS_API_TESTNET : BLOCKS_API_MAINNET;
 export const blockClient = new ApolloClient({
   link: new HttpLink({
-    uri: BLOCKS_API_TESTNET,
+    uri: _blocksApi,
   }),
   cache: new InMemoryCache(),
 });
 
+const _api = currentConnection === 'testnet' ? APIURL : API_MAINNET;
 export const client = new ApolloClient({
   link: new HttpLink({
-    uri: APIURL,
+    uri: _api,
   }),
   cache: new InMemoryCache(),
   shouldBatch: true,
