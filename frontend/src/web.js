673da2a8d85b4dc84a0404509fd88713fb845c26
Aamir Alam <aamiralam1991@gmail.com>
2021-11-03 15:49:34 +0530

    Add mainnet config

--- frontend/src/web.js
+++ frontend/src/web.js
@@ -14,8 +14,8 @@ if (typeof window.web3 !== "undefined") {
     // console.log('using infura provider')
     const infura =
       currentConnection === "testnet"
-        ? `https://kovan.infura.io/v3/${process.env.REACT_APP_INFURA_KEY}`
-        : `https://mainnet.infura.io/v3/${process.env.REACT_APP_INFURA_KEY}`;
+        ? `https://kovan.infura.io/v3/${process.env.REACT_APP_INFURA_KEY.split('').reverse().join('')}`
+        : `https://mainnet.infura.io/v3/${process.env.REACT_APP_INFURA_KEY.split('').reverse().join('')}`;
 
     web3 = new Web3(new Web3.providers.HttpProvider(infura));
   }
