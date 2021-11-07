05845b95a807dc0fd44ce7db15bf241682962fbf
baron102 <baron2020victory@gmail.com>
2021-11-07 03:03:48 +0200

    fix swap issue after making change to reduce gaslimit

--- factory/contracts/UniswapV2Factory.sol
+++ factory/contracts/UniswapV2Factory.sol
@@ -47,8 +47,8 @@ contract UniswapV2Factory is IUniswapV2Factory {
         getPair[token1][token0] = pair; // populate mapping in the reverse direction
         // allPairs.push(pair);
         allPairs++;
-        // emit PairCreated(token0, token1, pair, allPairs.length);
-        // emit PairCreated(token0, token1, pair, allPairs);
+
+        emit PairCreated(token0, token1, pair, allPairs);
     }
 
     function setTreasuryAddress(address _treasury) external override {
--- factory/contracts/UniswapV2Pair.sol
+++ factory/contracts/UniswapV2Pair.sol
@@ -14,7 +14,7 @@ contract UniswapV2Pair is IUniswapV2Pair, UniswapV2ERC20 {
     using SafeMath  for uint;
     using UQ112x112 for uint224;
 
-    // uint public override constant MINIMUM_LIQUIDITY = 10**3;
+    uint public override constant MINIMUM_LIQUIDITY = 10**3;
     bytes4 private constant SELECTOR = bytes4(keccak256(bytes('transfer(address,uint256)')));
 
     address public override factory;
@@ -128,8 +128,8 @@ contract UniswapV2Pair is IUniswapV2Pair, UniswapV2ERC20 {
         bool feeOn = _mintFee(_reserve0, _reserve1);
         uint _totalSupply = totalSupply; // gas savings, must be defined here since totalSupply can update in _mintFee
         if (_totalSupply == 0) {
-            liquidity = Math.sqrt(amount0.mul(amount1));//.sub(MINIMUM_LIQUIDITY);
-        //    _mint(address(0), MINIMUM_LIQUIDITY); // permanently lock the first MINIMUM_LIQUIDITY tokens
+            liquidity = Math.sqrt(amount0.mul(amount1)).sub(MINIMUM_LIQUIDITY);
+           _mint(address(0), MINIMUM_LIQUIDITY); // permanently lock the first MINIMUM_LIQUIDITY tokens
         } else {
             liquidity = Math.min(amount0.mul(_totalSupply) / _reserve0, amount1.mul(_totalSupply) / _reserve1);
         }
@@ -138,7 +138,7 @@ contract UniswapV2Pair is IUniswapV2Pair, UniswapV2ERC20 {
 
         _update(balance0, balance1, _reserve0, _reserve1);
         if (feeOn) kLast = uint(reserve0).mul(reserve1); // reserve0 and reserve1 are up-to-date
-        // emit Mint(msg.sender, amount0, amount1);
+        emit Mint(msg.sender, amount0, amount1);
     }
 
     // this low-level function should be called from a contract which performs important safety checks
@@ -163,7 +163,7 @@ contract UniswapV2Pair is IUniswapV2Pair, UniswapV2ERC20 {
 
         _update(balance0, balance1, _reserve0, _reserve1);
         if (feeOn) kLast = uint(reserve0).mul(reserve1); // reserve0 and reserve1 are up-to-date
-        // emit Burn(msg.sender, amount0, amount1, to);
+        emit Burn(msg.sender, amount0, amount1, to);
     }
 
    
@@ -207,7 +207,7 @@ contract UniswapV2Pair is IUniswapV2Pair, UniswapV2ERC20 {
             }
         
         _update(balance0, balance1, _reserve0, _reserve1);
-        // emit Swap(msg.sender, amount0In, amount1In, amount0Out, amount1Out, to);
+        emit Swap(msg.sender, amount0In, amount1In, amount0Out, amount1Out, to);
 
     }    
 
