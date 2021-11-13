94b8f78549d87a7073d29a4360e58d50b3c7ea05
Aamir Alam <aamiralam1991@gmail.com>
2021-11-13 13:52:55 +0530

    Add subgraph mainnet setup

--- subgraph/src/mappings/pricing.ts
+++ subgraph/src/mappings/pricing.ts
@@ -1,27 +1,27 @@
 /* eslint-disable prefer-const */
 import { Pair, Token, Bundle } from '../../generated/schema'
 import { BigDecimal, Address, BigInt } from '@graphprotocol/graph-ts/index'
-import { ZERO_BD, factoryContract, ADDRESS_ZERO, ONE_BD, UNTRACKED_PAIRS } from './helpers'
+import { ZERO_BD, factoryContract, ADDRESS_ZERO, ONE_BD, UNTRACKED_PAIRS, testing } from './helpers'
 
-const WETH_ADDRESS = '0xc778417e063141139fce010982780140aa0cd5ab'
+const WETH_ADDRESS = testing ? '0xc778417e063141139fce010982780140aa0cd5ab' : '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2'
 // const USDC_WETH_PAIR = '0xb4e16d0168e52d35cacd2c6185b44281ec28c9dc' // created 10008355
 // const DAI_WETH_PAIR = '0xa478c2975ab1ea89e8196811f51a7b7ade33eb11' // created block 10042267
-const USDT_WETH_PAIR = '0xf0c8C846Fe1Ff60084F0c8fb03310Ea02e57Ed30' //'0x0d4a11d5eeaac28ec3f61d100daf4d40471f1852' // created block 10093341
+const USDT_WETH_PAIR = testing ? '0xE3419211dccfb659c6d967a290Fbe5F2eBFA241a' : '0xB264a701ace313e052A24BE79D7Cf96Ed8edEB70'  // created block 10093341
 
 
 export function getEthPriceInUSD(): BigDecimal {
   // fetch eth prices for each stablecoin
   // let daiPair = Pair.load(DAI_WETH_PAIR) // dai is token0
   // let usdcPair = Pair.load(USDC_WETH_PAIR) // usdc is token0
-  let usdtPair = Pair.load( Address.fromString(USDT_WETH_PAIR).toHexString() ) // usdt is token1
+  let usdtPair = Pair.load(Address.fromString(USDT_WETH_PAIR).toHexString()) // usdt is token1
 
 
 
   //eth price only from usdt eth pair
-   if (usdtPair !== null) {
-    return usdtPair.token0Price
+  if (usdtPair !== null) {
+    return usdtPair.token1Price
   } else {
-    return BigDecimal.fromString('0') ;
+    return BigDecimal.fromString('0');
   }
 
   // all 3 have been created
@@ -54,33 +54,36 @@ export function getEthPriceInUSD(): BigDecimal {
 }
 
 // token where amounts should contribute to tracked volume and liquidity
-let WHITELIST: string[] = [
+let WHITELIST: string[] = testing ? [
   "0xc778417e063141139fce010982780140aa0cd5ab", //WETH_ADDRESS_TESTNET
-  "0x117e41ec3ec246873d69bfa5659b8eb209e687d8", // usdt testnet
+  "0xe687b0a94c3a20540552d981cd311a6812759df8", // usdt testnet
   "0x4dbcdf9b62e891a7cec5a2568c3f4faf9e8abe2b", // usdc testnet
   "0xcc521406C8F796169DCe1D10bDe6AaA60847FB63", // 1INCH testnet
-  "0xf6c9FF0543f932178262DF8C81A12A3132129b51" // pbr address testnet
-  // '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2', // WETH
-  // '0x6b175474e89094c44da98b954eedeac495271d0f', // DAI
-  // '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48', // USDC
-  // '0xdac17f958d2ee523a2206206994597c13d831ec7', // USDT
-  // '0x0000000000085d4780b73119b644ae5ecd22b376', // TUSD
-  // '0x5d3a536e4d6dbd6114cc1ead35777bab948e3643', // cDAI
-  // '0x39aa39c021dfbae8fac545936693ac917d5e7563', // cUSDC
-  // '0x86fadb80d8d2cff3c3680819e4da99c10232ba0f', // EBASE
-  // '0x57ab1ec28d129707052df4df418d58a2d46d5f51', // sUSD
-  // '0x9f8f72aa9304c8b593d555f12ef6589cc3a579a2', // MKR
-  // '0xc00e94cb662c3520282e6f5717214004a7f26888', // COMP
-  // '0x514910771af9ca656af840dff83e8264ecf986ca', //LINK
-  // '0x960b236a07cf122663c4303350609a66a7b288c0', //ANT
-  // '0xc011a73ee8576fb46f5e1c5751ca3b9fe0af2a6f', //SNX
-  // '0x0bc529c00c6401aef6d220be8c6ea1667f6ad93e', //YFI
-  // '0xdf5e0e81dff6faf3a7e52ba697820c5e32d806a8', // yCurv
-  // '0x853d955acef822db058eb8505911ed77f175b99e', // FRAX
-  // '0xa47c8bf37f92abed4a126bda807a7b7498661acd', // WUST
-  // '0x1f9840a85d5af5bf1d1762f925bdaddc4201f984', // UNI
-  // '0x2260fac5e5542a773aa44fbcfedf7c193bc2c599' // WBTC
+  "0xf6c9FF0543f932178262DF8C81A12A3132129b51", // pbr address testnet
+] : [
+  '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2', // WETH
+  '0x6b175474e89094c44da98b954eedeac495271d0f', // DAI
+  '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48', // USDC
+  '0xdac17f958d2ee523a2206206994597c13d831ec7', // USDT
+  '0x298d492e8c1d909D3F63Bc4A36C66c64ACB3d695', // PBR
+  '0x0000000000085d4780b73119b644ae5ecd22b376', // TUSD
+  '0x5d3a536e4d6dbd6114cc1ead35777bab948e3643', // cDAI
+  '0x39aa39c021dfbae8fac545936693ac917d5e7563', // cUSDC
+  '0x86fadb80d8d2cff3c3680819e4da99c10232ba0f', // EBASE
+  '0x57ab1ec28d129707052df4df418d58a2d46d5f51', // sUSD
+  '0x9f8f72aa9304c8b593d555f12ef6589cc3a579a2', // MKR
+  '0xc00e94cb662c3520282e6f5717214004a7f26888', // COMP
+  '0x514910771af9ca656af840dff83e8264ecf986ca', //LINK
+  '0x960b236a07cf122663c4303350609a66a7b288c0', //ANT
+  '0xc011a73ee8576fb46f5e1c5751ca3b9fe0af2a6f', //SNX
+  '0x0bc529c00c6401aef6d220be8c6ea1667f6ad93e', //YFI
+  '0xdf5e0e81dff6faf3a7e52ba697820c5e32d806a8', // yCurv
+  '0x853d955acef822db058eb8505911ed77f175b99e', // FRAX
+  '0xa47c8bf37f92abed4a126bda807a7b7498661acd', // WUST
+  '0x1f9840a85d5af5bf1d1762f925bdaddc4201f984', // UNI
+  '0x2260fac5e5542a773aa44fbcfedf7c193bc2c599' // WBTC
 ]
+
 
 // minimum liquidity required to count towards tracked volume for pairs with small # of Lps
 let MINIMUM_USD_THRESHOLD_NEW_PAIRS = BigDecimal.fromString('0')
@@ -92,7 +95,7 @@ let MINIMUM_LIQUIDITY_THRESHOLD_ETH = BigDecimal.fromString('0')
  * Search through graph to find derived Eth per token.
  * @todo update to be derived ETH (add stablecoin estimates)
  **/
- export function findEthPerToken(token: Token): BigDecimal {
+export function findEthPerToken(token: Token): BigDecimal {
   if (token.id == WETH_ADDRESS) {
     return ONE_BD
   }
