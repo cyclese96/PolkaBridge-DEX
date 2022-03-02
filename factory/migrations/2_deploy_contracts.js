5119aaca4f8c30bd5d753a981e884c2935a0bf52
baron102 <baron2020victory@yandex.com>
2022-03-01 19:49:12 -0800

    make change on deploy js

--- factory/migrations/2_deploy_contracts.js
+++ factory/migrations/2_deploy_contracts.js
@@ -1,5 +1,6 @@
-const UniswapV2Pair = artifacts.require("UniswapV2Pair");
 const UniswapV2Factory = artifacts.require("UniswapV2Factory");
+const UniswapV2Pair = artifacts.require("UniswapV2Pair");
+const Multicall2 = artifacts.require("Multicall2");
 
 module.exports = function (deployer) {
   let owner = "0x57866ed63ca5f9744cef9aa270bd1f1dce935831";
@@ -9,6 +10,7 @@ module.exports = function (deployer) {
   let maintreasury = "0xAa9b97cf7E4c52D028b854F2610aac18F60c86F5";
   deployer.deploy(UniswapV2Factory, owner, maintreasury);
   deployer.deploy(UniswapV2Pair);//, owner, treasury);
+  deployer.deploy(Multicall2);
 
   // console.log("Factory address is ", factoryAddress);
   // console.log("Pair address is ", pairAddress);
